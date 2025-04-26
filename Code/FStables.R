20d7a13b0be803cacc1d6b63b33a04d8bdc23d0e
SaraRiyami <sarasalimriyami@gmail.com>
2025-04-26 18:44:01 -0400

    Create freq and summary file
    File for freq and summary tables

---
+++ Code/FStables.R
@@ -0,0 +1,16 @@
+# Creating summary and frequency tables ------
+
+url <- "https://raw.githubusercontent.com/Stat184-Spring2025/Sec4_FP_Layan_Sara/main/Data/JoinedIMDB.csv"
+JoinedIMDB <- read.csv(url, header = TRUE)
+
+# Inspecting a frequency table ----
+
+
+
+
+
+
+
+
+
+
