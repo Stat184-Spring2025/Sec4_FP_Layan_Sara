cece6b14d5271aaea5d942f78bcf353982b7ae6f
SaraRiyami <sarasalimriyami@gmail.com>
2025-04-26 16:45:44 -0400

    Tidying the IMDB rating data
    Removed junks, and separated origins and genres then changed duration to minutes.

--- Tidying_Data.R
+++ Tidying_Data.R
@@ -1,34 +1,55 @@
-# Install and load all packages 
+# Load packages 
 library(tidyverse)
 library(dplyr)
-library(janitor)
-library(knitr)
-library(kableExtra)
-library(dcData)
-library(ggplot2)
-
 
 # Read the data 
 url1 <- "https://raw.githubusercontent.com/Stat184-Spring2025/Sec4_FP_Layan_Sara/main/Data/IMDB%20Gross.csv"
 IMDB_Gross <- read.csv(url1, header = TRUE)
 
 url2 <- "https://raw.githubusercontent.com/Stat184-Spring2025/Sec4_FP_Layan_Sara/main/Data/imdb_top_250_cost.csv"
-IMDB_Rating <- read.csv(url2, header = TRUE)
+IMDB_RatingRaw <- read.csv(url2, header = TRUE)
 
 # View and inspect the data
 #view(IMDB_Gross)
-#view(IMDB_Rating)
+#view(IMDB_RatingRaw)
 #print(colnames(IMDB_Gross))
-print(colnames(IMDB_Rating))
+#print(colnames(IMDB_RatingRaw))
 
 # Remove unwanted columns/rows
 IMDB_Gross <- IMDB_Gross %>%
   select(-c("Release.Date", "Domestic.Weekend.Gross", "Domestic.Weekend",
             "Domestic.Weekend.Gross.Date"))
 
-IMDB_Rating <-IMDB_Rating %>%
-  select(-c("X", "IMDB.link"))
 
-
+# Tidy the IMDB Rating data ----
+IMDB_Rating <-IMDB_RatingRaw %>%
+  #Remove the unwanted junk columns 
+  select(-c("X", "IMDB.link")) %>%
+  #Take the first 3 genres of the movies to make it simpler 
+  separate(Genre, into = c("Main_Genre", "Subgenre 1", "Subgenre 2"), 
+           sep = "\\|", fill = "right", extra = "drop") %>%
+  #Remove extra spaces between genres 
+  mutate(across(
+    c("Main_Genre", "Subgenre 1", "Subgenre 2"),
+    ~ trimws(.))) %>%
+  #Separate the Origin country and take upto 3 origin countries
+  separate(Origin, into = c("Main_Origin", "Suborigin 1", "Suborigin 2"), 
+           sep = "\\|", fill = "right", extra = "drop") %>%
+  #Remove extra spaces between genres
+  mutate(across(c("Main_Origin", "Suborigin 1", "Suborigin 2"), 
+                ~ trimws(.))) %>%
+  #Convert Duration to total minutes instead of hours and minutes
+  mutate(
+    #extract hours before 'h'
+    Hours = str_extract(Duration, "\\d+h") %>% str_remove("h") %>% 
+      as.numeric(),
+    #extract minutes before 'min'
+    Minutes = str_extract(Duration, "\\d+min") %>% str_remove("min") %>% 
+      as.numeric(),
+    Hours = ifelse(is.na(Hours), 0, Hours),      # if no hours, set to 0
+    Minutes = ifelse(is.na(Minutes), 0, Minutes),  # if no minutes, set to 0
+    Duration_min = Hours * 60 + Minutes             # total minutes
+  ) %>%
+  select(-Duration, -Hours, -Minutes, )
 
 
