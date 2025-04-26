a605b65dbfde5e30a8ecc8fc84326549fa55c243
LayanAlBusaidi <layan.321@hotmail.com>
2025-04-26 17:27:04 -0400

    Tidying Movies dataset
    Remove unnecessary columns

--- Tidying_Data.R
+++ Tidying_Data.R
@@ -1,28 +1,33 @@
-# Load packages 
+# Load Packages ----
 library(tidyverse)
 library(dplyr)
 
-# Read the data 
-url1 <- "https://raw.githubusercontent.com/Stat184-Spring2025/Sec4_FP_Layan_Sara/main/Data/IMDB%20Gross.csv"
-IMDB_Gross <- read.csv(url1, header = TRUE)
+
+# Read Data ----
+url1 <- "https://raw.githubusercontent.com/Stat184-Spring2025/Sec4_FP_Layan_Sara/main/Data/Budget_Revnue.csv"
+MoviesGrossRaw <- read.csv(url1, header = TRUE)
 
 url2 <- "https://raw.githubusercontent.com/Stat184-Spring2025/Sec4_FP_Layan_Sara/main/Data/imdb_top_250_cost.csv"
-IMDB_RatingRaw <- read.csv(url2, header = TRUE)
+IMDBRatingRaw <- read.csv(url2, header = TRUE)
 
-# View and inspect the data
-#view(IMDB_Gross)
+
+# View and inspect data ----
+#view(MoviesGrossRaw)
 #view(IMDB_RatingRaw)
-#print(colnames(IMDB_Gross))
-#print(colnames(IMDB_RatingRaw))
 
-# Remove unwanted columns/rows
-IMDB_Gross <- IMDB_Gross %>%
-  select(-c("Release.Date", "Domestic.Weekend.Gross", "Domestic.Weekend",
-            "Domestic.Weekend.Gross.Date"))
+#print(colnames(IMDBGrossRaw))
+#print(colnames(IMDBRatingRaw))
+
+
+# Tidying Movies Gross Data ----
+MoviesGrossTidy <- MoviesGrossRaw %>%
+  select(-c("genre","released","score","votes","director","country"))
+
+#view(MoviesGrossTidy)
 
 
 # Tidy the IMDB Rating data ----
-IMDB_Rating <-IMDB_RatingRaw %>%
+IMDBRatingTidy <- IMDBRatingRaw %>%
   #Remove the unwanted junk columns 
   select(-c("X", "IMDB.link", "Duration")) %>%
   #Take the first 3 genres of the movies to make it simpler 
